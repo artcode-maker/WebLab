cfc14bdfca37b92c3fdec4a44cb9d843eaf17561
Artem Klinduhov <artemklinduhov7@gmail.com>
2020-10-04 22:08:00 +0300

    Laboratory work 2

--- WebLab/Controllers/HomeController.cs
+++ WebLab/Controllers/HomeController.cs
@@ -6,32 +6,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebLab.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WebLab.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private List<ListDemo> listDemo;
+        public HomeController()
         {
-            _logger = logger;
+            listDemo = new List<ListDemo>
+            {
+                new ListDemo { ListItemValue = 1, ListItemText = "Item 1"},
+                new ListDemo { ListItemValue = 2, ListItemText = "Item 2"},
+                new ListDemo { ListItemValue = 3, ListItemText = "Item 3"}
+            };
         }
-
-        public IActionResult Index()
+        public ViewResult Index()
         {
+            ViewData["Text"] = "Лабораторная работа 2";
+            ViewData["lst"] = new SelectList(listDemo, "ListItemValue", "ListItemText");
             return View();
         }
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
     }
 }
