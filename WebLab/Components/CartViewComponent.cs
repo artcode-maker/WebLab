fbf58eaf4b6b8f5ae3daa5ec393a1de81b6dc3e0
Artem Klinduhov <artemklinduhov7@gmail.com>
2020-10-06 12:18:54 +0300

    laboratory work 3

---
+++ WebLab/Components/CartViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebLab.Models;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Html;
+namespace WebLab.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebLab/Components/MenuViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebLab.Models;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Html;
+
+namespace WebLab.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private List<MenuItem> _menuItems = new List<MenuItem>
+        {
+            new MenuItem{ Controller="Home", Action="Index", Text="Lab 2"},
+            new MenuItem{ Controller="Product", Action="Index", Text="Каталог"},
+            new MenuItem{ IsPage=true, Area="Admin", Page="/Index", Text="Администрирование"}
+        };
+
+        public IViewComponentResult Invoke()
+        {
+            var controller = ViewContext.RouteData.Values["controller"];
+            var area = ViewContext.RouteData.Values["area"];
+            var page = ViewContext.RouteData.Values["page"];
+            foreach (var item in _menuItems)
+            {
+                bool _matchController = controller?.Equals(item.Controller) ?? false;
+                var _matchArea = area?.Equals(item.Area) ?? false;
+                if (_matchController || _matchArea)
+                {
+                    item.Active = "active";
+                }
+            }
+            return View(_menuItems);
+        }
+    }    
+}
